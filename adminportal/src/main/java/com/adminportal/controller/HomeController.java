92e260edd6d2026fdda764837354f9c1f511e0d5
khmus <khmus@LAPTOP-T7QDPOTD.Speedport_W_724V_01011603_06_002>
2019-02-25 08:45:53 +0100

    login page

--- adminportal/src/main/java/com/adminportal/controller/HomeController.java
+++ adminportal/src/main/java/com/adminportal/controller/HomeController.java
@@ -6,10 +6,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
+	
+	
 	@RequestMapping("/")
+	public String index() {
+		return "redirect:/home";
+	}
+	
+	@RequestMapping("/home")
 	public String home() {
-		System.out.println("");
 		return "home";
+	}
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
 	}
 
 }
